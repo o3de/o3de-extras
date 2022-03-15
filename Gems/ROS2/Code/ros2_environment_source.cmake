5c4eef4638dbe1c7ae292ee0e2b01107345be094
Adam Dąbrowski <adam.dabrowski@robotec.ai>
2022-03-15 10:54:55 +0100

    CMake function to handle ros2 deps
    Signed-off-by: Adam Dąbrowski <adam.dabrowski@robotec.ai>

--- Gems/ROS2/Code/ros2_environment_source.cmake
+++ Gems/ROS2/Code/ros2_environment_source.cmake
@@ -1,31 +1,34 @@
 
+function(target_depends_on_ros2_packages TARGET_NAME)
+    if (NOT DEFINED ENV{ROS_DISTRO})
+        message(WARNING, "To build ROS2 Gem a ROS distribution needs to be sourced, but none detected")
+    endif()
+    #message("Building ROS2 Gem with ros2 $ENV{ROS_DISTRO}")
 
-if (NOT DEFINED ENV{ROS_DISTRO})
-    message(WARNING, "To build ROS2 Gem a ROS distribution needs to be source, but none detected")
-endif()
-message("Building ROS2 Gem with ros2 $ENV{ROS_DISTRO}")
+    #TODO - compare to previous env since we need to rerun cmake if we source a different ros2 env!
+    #TODO - can be done with a file that is in CONFIGURE_DEPENDS so that a change triggers build
+    if (NOT DEFINED ENV{AMENT_PREFIX_PATH})
+        message(WARNING, "$ENV{AMENT_PREFIX_PATH} missing from env. ROS 2 needs to be sourced to build ROS2 Gem")
+    endif()
+    set (_ament_prefix_path "$ENV{AMENT_PREFIX_PATH}")
 
-#TODO - compare to previous env since we need to rerun cmake if we source a different ros2 env!
-#TODO - can be done with a file that is in CONFIGURE_DEPENDS so that a change triggers build
-if (NOT DEFINED ENV{AMENT_PREFIX_PATH})
-    message(WARNING, "$ENV{AMENT_PREFIX_PATH} missing from env. ROS 2 needs to be sourced to build ROS2 Gem")
-endif()
-set (AMENT_PREFIX_PATH "$ENV{AMENT_PREFIX_PATH}")
-
-# ros2 directories with libraries, e.g. /opt/ros/galactic/lib, locally built custom packages etc.
-set(ros2_library_directories)
-set(ros2_include_directories)
-foreach(ros2_packages_path IN LISTS AMENT_PREFIX_PATH)
-    #message("including packages: ${ros2_packages_path}")
-    string(REPLACE ":" ";" ros2_packages_path ${ros2_packages_path})
-    list(APPEND ros2_library_directories "${ros2_packages_path}/lib")
-    list(APPEND ros2_include_directories "${ros2_packages_path}/include")
-endforeach()
-
-find_package(rclcpp REQUIRED)
-find_package(std_msgs REQUIRED)
-set(ros2_libraries ${rclcpp_LIBRARIES} ${std_msgs_LIBRARIES})
-
-#message("Found libraries: ${ros2_libraries}")
-#message("Found include directories: ${ros2_include_directories}")
-#message("Library directories: ${ros2_library_directories}")
+    # ros2 directories with libraries, e.g. /opt/ros/galactic/lib, locally built custom packages etc.
+    set(_ros2_library_directories)
+    set(_ros2_include_directories)
+    set(_ros2_package_libraries)
+    foreach(_ros2_packages_path IN LISTS _ament_prefix_path)
+        #message("including packages: ${ros2_packages_path}")
+        string(REPLACE ":" ";" _ros2_packages_path ${_ros2_packages_path})
+        list(APPEND _ros2_library_directories "${_ros2_packages_path}/lib")
+        list(APPEND _ros2_include_directories "${_ros2_packages_path}/include")
+    endforeach()
+    foreach(_package IN LISTS ARGN)
+        message("processing package: ${_package}")
+        find_package(${_package} REQUIRED)
+        list(APPEND _ros2_package_libraries "${${_package}_LIBRARIES}")
+    endforeach()
+    #message("Found include directories: ${_ros2_include_directories}")
+    #message("Found libraries: ${_ros2_package_libraries}")
+    target_include_directories(${TARGET_NAME} PUBLIC ${_ros2_include_directories})
+    target_link_libraries(${TARGET_NAME} PUBLIC ${_ros2_package_libraries})
+endfunction()
