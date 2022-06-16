e3cef546241ff34093c8fabd98dbdab4119b82c2
Michał Drwięga <drwiega.michal@gmail.com>
2022-06-16 16:50:28 +0200

    Add URDF to FBX conversion (#83)
    Changes:
    - handle materials
    - created tests: with two links and joint and with simple robot
    - depth first search in URDF links tree and add relations based on
    joints
    - switched to AZStd containers
    - fixed memory allocation in tests
    
    Signed-off-by: Michał Drwięga <drwiega.michal@gmail.com>

--- Gems/ROS2/Code/Source/URDF/UrdfToFbxConverter.cpp
+++ Gems/ROS2/Code/Source/URDF/UrdfToFbxConverter.cpp
@@ -9,22 +9,96 @@
 #include "UrdfToFbxConverter.h"
 
 #include <AzCore/std/string/string.h>
-
-#include "UrdfParser.h"
-#include "FbxGenerator.h"
+#include <AzCore/Console/Console.h>
 
 namespace ROS2
 {
-    AZStd::string UrdfToFbxConverter::ConvertUrdfToFbx(const AZStd::string & urdfString)
+    AZStd::string UrdfToFbxConverter::Convert(const AZStd::string & urdfString)
     {
-        // TODO: Add implementation
+        // 1. Parse URDF
+        const auto urdf = UrdfParser::Parse(urdfString.data());
 
-        // Workflow
-        // 1. Parse URDF with UrdfParser
-        // 2. Create FBX file with FbxGenerator
-        // 3. Add each object from URDF based structure to FBX generator.
+        // 2. Add materials to FBX
+        AddMaterialsToFbxGenerator(urdf);
 
-        return "";
+        // 3. Add links from URDF based structure to FBX generator in Depth First order
+        const auto root = urdf->getRoot();
+
+        std::stack<urdf::Link> stack;
+        stack.push(*root);
+
+        while(!stack.empty())
+        {
+            const auto link = stack.top();
+            stack.pop();
+
+            AddLinkToFbxGenerator(link);
+
+            // Add childs to stack
+            for (const auto & child : link.child_links)
+            {
+                stack.push(*child);
+            }
+        }
+
+        return m_generator.GetFbxString().data();
+    }
+
+    AZStd::string UrdfToFbxConverter::ConvertAndSaveToFile(
+        const AZStd::string & urdfString, const AZStd::string & filePath)
+    {
+        const auto fbxContent = Convert(urdfString);
+        m_generator.SaveToFile(filePath.data());
+
+        return fbxContent;
+    }
+
+    void UrdfToFbxConverter::AddLinkToFbxGenerator(const urdf::Link & link)
+    {
+        const auto linkGeometry = link.visual->geometry;
+
+        if (linkGeometry->type == urdf::Geometry::BOX)
+        {
+            auto boxGeometry = std::dynamic_pointer_cast<urdf::Box>(linkGeometry);
+            const double cubeSize = boxGeometry->dim.x; // TODO: Handle box in FBX instead of cube
+            AZStd::string materialName(link.visual->material_name.c_str());
+            auto objectId = m_generator.AddCubeObject(link.name.c_str(), cubeSize, m_materialNamesToIds[materialName]);
+            m_objectNameToObjectId[link.name.c_str()] = objectId;
+        }
+        else
+        {
+            AZ_Warning(__func__, false, "Only box geometry is supported.");
+        }
+
+        // Set proper relations between links (only root has no parent)
+        // TODO: handle joints tranformations
+        if (const auto & parent = link.getParent())
+        {
+            const auto parentId = m_objectNameToObjectId[parent->name.c_str()];
+            const auto childId = m_objectNameToObjectId[link.name.c_str()];
+            m_generator.SetRelationBetweenObjects(parentId, childId);
+        }
+    }
+
+    void UrdfToFbxConverter::AddMaterialsToFbxGenerator(const urdf::ModelInterfaceSharedPtr & urdfModel)
+    {
+        if (!urdfModel)
+        {
+            AZ_Error(__func__, false, "Missing URDF model.");
+            return;
+        }
+
+        for (const auto & e : urdfModel->materials_)
+        {
+            const auto material = e.second;
+            const AZStd::string materialName(material->name.c_str());
+            const auto materialColor = material->color;
+            const Fbx::Color fbxColor(materialColor.r, materialColor.g, materialColor.b);
+            const auto materialId = m_generator.AddMaterial(materialName, fbxColor);
+            m_materialNamesToIds[materialName] = materialId;
+
+            AZ_Printf(__func__, "Add new material: %s", materialName.c_str());
+        }
     }
 
 } // namespace ROS2
--- Gems/ROS2/Code/Source/URDF/UrdfToFbxConverter.h
+++ Gems/ROS2/Code/Source/URDF/UrdfToFbxConverter.h
@@ -10,13 +10,32 @@
 
 #include <AzCore/std/string/string.h>
 
+#include <AzCore/std/string/string.h>
+#include <AzCore/std/containers/map.h>
+#include <AzCore/Memory/SystemAllocator.h>
+
+#include "FbxGenerator.h"
+#include "UrdfParser.h"
+
 namespace ROS2
 {
     //! Class for conversion from URDF to Filmbox (.fbx) files
     class UrdfToFbxConverter
     {
     public:
-        AZStd::string ConvertUrdfToFbx(const AZStd::string & urdfString);
+        AZ_CLASS_ALLOCATOR(UrdfToFbxConverter, AZ::SystemAllocator, 0);
+
+        AZStd::string Convert(const AZStd::string & urdfString);
+
+        AZStd::string ConvertAndSaveToFile(const AZStd::string & urdfString, const AZStd::string & filePath);
+
+    private:
+        void AddLinkToFbxGenerator(const urdf::Link & urdfLink);
+        void AddMaterialsToFbxGenerator(const urdf::ModelInterfaceSharedPtr & urdfModel);
+
+        Fbx::FbxGenerator m_generator;
+        AZStd::map<AZStd::string, Id> m_materialNamesToIds;
+        AZStd::map<AZStd::string, Id> m_objectNameToObjectId;
     };
 
 } // namespace ROS2
