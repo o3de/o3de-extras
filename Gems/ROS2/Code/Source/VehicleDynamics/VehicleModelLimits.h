7f8e6d374e4eff3c485c46ee572f47662ea2cc4d
Michał Pełka <michal.pelka@robotec.ai>
2023-01-24 14:07:06 +0100

    Add skid steering and adjust vehicle dynamics. (#86)
    * Skid steering control component.
    Adjust Vehicle Dynamic code to use PhysX joint requests in ROS2 Gem
    
    Changed existing vehicle dynamic code in ROS2 Gem to use JointRequests
    from PhysX Gem.
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    * adjusted to review
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    * adjusted to review
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    * Adjusted to reviews.
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    Signed-off-by: Daniele Morra <morradaniele@ymail.com>

--- Gems/ROS2/Code/Source/VehicleDynamics/VehicleModelLimits.h
+++ Gems/ROS2/Code/Source/VehicleDynamics/VehicleModelLimits.h
@@ -7,26 +7,35 @@
  */
 #pragma once
 
+#include "VehicleInputs.h"
 #include <AzCore/RTTI/TypeInfo.h>
 #include <AzCore/Serialization/SerializeContext.h>
 
 namespace ROS2::VehicleDynamics
 {
     //! A structure holding limits of vehicle, including speed and steering limits
-    struct VehicleModelLimits
+    class VehicleModelLimits
     {
     public:
-        AZ_TYPE_INFO(VehicleModelLimits, "{76DA392D-64BB-45A8-BC90-84AAE7901991}");
+        AZ_RTTI(VehicleModelLimits, "{76DA392D-64BB-45A8-BC90-84AAE7901991}");
         VehicleModelLimits() = default;
+        virtual ~VehicleModelLimits() = default;
         static void Reflect(AZ::ReflectContext* context);
 
+        //! Limit input state to values that are possible for model.
+        //! @param inputState Input state to filter.
+        //! @returns Filtered, pruned state.
+        virtual VehicleInputs LimitState(const VehicleInputs& inputState) const = 0;
+
+        //! Returns maximal permissible input states.
+        //! @returns VehicleInputsState
+        virtual VehicleInputs GetMaximumState() const = 0;
+
+    protected:
         //! Limit value with a symmetrical range.
         //! @param value Input value.
         //! @param absoluteLimit Limits for value (between -absoluteLimit and absoluteLimit).
         //! @returns A limited value. Always returns either value, -absoluteLimit or absoluteLimit.
         static float LimitValue(float value, float absoluteLimit);
-
-        float m_speedLimit = 10.0f; //!< [Mps] Applies to absolute value
-        float m_steeringLimit = 0.7f; //!< [rad] Applies to absolute value
     };
 } // namespace ROS2::VehicleDynamics
