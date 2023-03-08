4cf6edfcd2fa2d0a7026b8c3025524fa01d8e1d3
Michał Pełka <michal.pelka@robotec.ai>
2023-03-08 12:04:59 +0100

    Change cmake to treat urdfdom as normal package, not ament package. Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ROS2/Code/ros2_target_depends.cmake
+++ Gems/ROS2/Code/ros2_target_depends.cmake
@@ -3,26 +3,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-function(target_depends_on_ros2_package TARGET_NAME )
-    list (GET ARGN 0 _package)
+function(target_depends_on_ros2_package TARGET_NAME)
+    list(GET ARGN 0 _package)
     find_package(${ARGN})
     include(${${_package}_DIR}/${_package}Config.cmake OPTIONAL)
-    include(${${_package}_DIR}/${_package}config.cmake OPTIONAL)
-    include(${${_package}_DIR}/${_package}-config.cmake OPTIONAL)
-    include(${${_package}_DIR}/${_package}-Config.cmake OPTIONAL)
-    if( ${${_package}_FOUND} )
+    if (${${_package}_FOUND_AMENT_PACKAGE})
         message(DEBUG "Package ${_package} was found (${${_package}_DIR}) version ${${_package}_VERSION} targets : ${${_package}_TARGETS}")
         target_link_libraries(${TARGET_NAME} PUBLIC ${${_package}_TARGETS})
-    endif()
+    else ()
+        message(FATAL_ERROR "Package ${_package} was found (${${_package}_DIR}), but package is not an Ament package.")
+    endif ()
 endfunction()
 
 function(target_depends_on_ros2_packages TARGET_NAME)
-    foreach(_package IN LISTS ARGN)
-        message(DEBUG "Processing package: ${_package}")
+    foreach (_package IN LISTS ARGN)
         target_depends_on_ros2_package(${TARGET_NAME} ${_package} REQUIRED)
-    endforeach()
+    endforeach ()
 endfunction()
-
-
-
-
