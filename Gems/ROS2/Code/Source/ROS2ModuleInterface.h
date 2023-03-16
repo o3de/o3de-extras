b7aa465dd021490470acac09f609ad609171e92e
wojtek-robotecai <wojciech.dudek@robotec.ai>
2023-03-16 19:13:24 +0100

    IMU implementation using XPhysics
    Signed-off-by: wojtek-robotecai <wojciech.dudek@robotec.ai>
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ROS2/Code/Source/ROS2ModuleInterface.h
+++ Gems/ROS2/Code/Source/ROS2ModuleInterface.h
@@ -13,6 +13,7 @@
 #include <Camera/ROS2CameraSensorComponent.h>
 #include <GNSS/ROS2GNSSSensorComponent.h>
 #include <Imu/ROS2ImuSensorComponent.h>
+#include <Imu/ROS2OldImuSensorComponent.h>
 #include <Lidar/LidarRegistrarSystemComponent.h>
 #include <Lidar/ROS2LidarSensorComponent.h>
 #include <Odometry/ROS2OdometrySensorComponent.h>
@@ -52,6 +53,7 @@ namespace ROS2
                   ROS2RobotImporterSystemComponent::CreateDescriptor(),
                   ROS2SensorComponent::CreateDescriptor(),
                   ROS2ImuSensorComponent::CreateDescriptor(),
+                  ROS2OldImuSensorComponent::CreateDescriptor(),
                   ROS2GNSSSensorComponent::CreateDescriptor(),
                   ROS2LidarSensorComponent::CreateDescriptor(),
                   ROS2OdometrySensorComponent::CreateDescriptor(),
