c5b3b90b0d2406d5ac766920f1637aa7072e23a9
Michał Pełka <michal.pelka@robotec.ai>
2023-03-20 16:10:40 +0100

    Fleet of Robots template (#186)
    * Introduce fleet robot template.
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    * New template name and some fixes
    
    - Changed template name
    - Updated `template.json` file
    - Added render passes folder
    - Some visual fixes in the main level
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Extended readme
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Added license file
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Update README.md
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Adjusted to review
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Reverted {BEGIN/END_LICENSE} removal
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Fix: missing entry
    
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    
    * Adjustments to review, minor changes to readme
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    * Apply suggestions from code review
    
    Co-authored-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>
    Signed-off-by: Michał Pełka <michalpelka@gmail.com>
    
    * Further adjusments to review.
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    
    ---------
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    Signed-off-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    Signed-off-by: Michał Pełka <michalpelka@gmail.com>
    Co-authored-by: Paweł Budziszewski <pawel.budziszewski@robotec.ai>
    Co-authored-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

---
+++ Templates/Ros2FleetRobotTemplate/Template/Gem/enabled_gems.cmake
@@ -0,0 +1,35 @@
+# {BEGIN_LICENSE}
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+#
+# {END_LICENSE}
+# This file is copied during engine registration. Edits to this file will be lost next
+# time a registration happens.
+
+set(ENABLED_GEMS
+    ${Name}
+    Atom
+    CameraFramework
+    DebugDraw
+    EditorPythonBindings
+    EMotionFX
+    ImGui
+    LyShine
+    PhysX
+    PrimitiveAssets
+    PrefabBuilder
+    ScriptCanvasPhysics
+    ScriptEvents
+    StartingPointInput
+    TextureAtlas
+    WhiteBox
+    DiffuseProbeGrid
+    Compression
+    ProteusRobot
+    WarehouseAssets
+    ROS2
+)
