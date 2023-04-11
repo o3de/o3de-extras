6cff8c3ceac434881f2e9f8e2d63f6dbaeb13f16
Adam Dabrowski <adam.dabrowski@robotec.ai>
2023-04-11 17:03:37 +0200

    documentation mainpage
    Signed-off-by: Adam Dabrowski <adam.dabrowski@robotec.ai>

--- Gems/ROS2/Code/Source/ROS2SystemComponent.h
+++ Gems/ROS2/Code/Source/ROS2SystemComponent.h
@@ -20,6 +20,21 @@
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
+/**
+ * \mainpage
+ *
+ * Welcome to the **Open 3D Engine (O3DE)** API Reference for the **ROS 2 Gem**!
+ *
+ * The Components and Classes of this Gem support direct interaction with the ROS 2 ecosystem.
+ * The Robot Operating System (ROS) middleware documentation links in this API Reference point to the most recent ROS distribution. If you are using an
+ * older version, you might need to navigate to the corresponding documentation pages for the distribution which you have installed.
+ *
+ * For the overview, features and usage of this Gem, please refer to [ROS 2 Gem Documentation](https://development--o3deorg.netlify.app/docs/user-guide/gems/reference/robotics/ros2/).
+ *
+ * o3de-doxygen-insert-table
+ *
+ */
+
 namespace ROS2
 {
     //! Central singleton-like System Component for ROS2 Gem.
