e334b09e5c6f4e852adf3d20f1acda9b5225afe0
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2023-07-04 12:24:17 +0200

    Fix crash after moveit action stop
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

--- Gems/ROS2/Code/Source/Manipulation/JointsTrajectoryComponent.cpp
+++ Gems/ROS2/Code/Source/Manipulation/JointsTrajectoryComponent.cpp
@@ -174,10 +174,9 @@ namespace ROS2
         m_followTrajectoryServer->PublishFeedback(feedback);
     }
 
-    AZ::Outcome<void, AZStd::string> JointsTrajectoryComponent::CancelTrajectoryGoal(TrajectoryResultPtr result)
+    AZ::Outcome<void, AZStd::string> JointsTrajectoryComponent::CancelTrajectoryGoal()
     {
         m_trajectoryGoal.trajectory.points.clear();
-        m_followTrajectoryServer->CancelGoal(result);
         m_trajectoryInProgress = false;
         return AZ::Success();
     }
@@ -193,6 +192,11 @@ namespace ROS2
         if (goalStatus == JointsTrajectoryRequests::TrajectoryActionStatus::Cancelled)
         {
             JointsManipulationRequestBus::Event(GetEntityId(), &JointsManipulationRequests::Stop);
+            auto result = std::make_shared<FollowJointTrajectoryActionServer::FollowJointTrajectory::Result>();
+            result->error_string = "User Cancelled";
+            result->error_code = FollowJointTrajectoryActionServer::FollowJointTrajectory::Result::SUCCESSFUL;
+            m_followTrajectoryServer->CancelGoal(result);
+            m_followTrajectoryServer->SetGoalSuccess();
             return;
         }
 
