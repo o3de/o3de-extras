d0df7e9b666d1e8172f158841ea67d64ab205472
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-07-21 14:54:13 -0500

    Adding the sdformat13 3rdParty library as a dependency the `ROS2.Editor.Static` TARGET.
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Gems/ROS2/Code/Platform/Linux/PAL_linux.cmake
+++ Gems/ROS2/Code/Platform/Linux/PAL_linux.cmake
@@ -7,3 +7,13 @@
 #
 
 set(PAL_TRAIT_BUILD_ROS2_GEM_SUPPORTED TRUE)
+
+if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+    ly_associate_package(PACKAGE_NAME sdformat-13.5.0-rev0-linux
+        TARGETS sdformat
+        PACKAGE_HASH 407a335c1fefc134e89b2dc2d1ffd6a47ea33546e77df4aaabf3edf8b3402ecb)
+elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
+    ly_associate_package(PACKAGE_NAME sdformat-13.5.0-rev0-linux-aarch64
+        TARGETS sdformat
+        PACKAGE_HASH a23977b262d6d80a5e1b22c24db27d25fe827c982296766c0f04ebf77ad0a217)
+endif()
