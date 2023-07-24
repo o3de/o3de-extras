2f5afe8fb62edeaa596c7f87c1aae247c2dc8cc9
lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>
2023-07-24 14:36:12 -0500

    Updating package hash for sdformat13 package for new builds which are built against the "Release" configuration.
    Signed-off-by: lumberyard-employee-dm <56135373+lumberyard-employee-dm@users.noreply.github.com>

--- Gems/ROS2/Code/Platform/Linux/PAL_linux.cmake
+++ Gems/ROS2/Code/Platform/Linux/PAL_linux.cmake
@@ -11,9 +11,9 @@ set(PAL_TRAIT_BUILD_ROS2_GEM_SUPPORTED TRUE)
 if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
     ly_associate_package(PACKAGE_NAME sdformat-13.5.0-rev0-linux
         TARGETS sdformat
-        PACKAGE_HASH 407a335c1fefc134e89b2dc2d1ffd6a47ea33546e77df4aaabf3edf8b3402ecb)
+        PACKAGE_HASH fd7f34bf9e9d20320ff29ae74ea0fce8b9f7688dd3c1dbd4b4b8a68d31b7d2ee)
 elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
     ly_associate_package(PACKAGE_NAME sdformat-13.5.0-rev0-linux-aarch64
         TARGETS sdformat
-        PACKAGE_HASH a23977b262d6d80a5e1b22c24db27d25fe827c982296766c0f04ebf77ad0a217)
+        PACKAGE_HASH ca0fedaa00d82f6b211a2ed94101f2dafb11fa2732c3955443db78325abda2a6)
 endif()
