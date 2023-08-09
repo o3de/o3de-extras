5e4a5763c4ebb14d98bf9ea9ab8206d4180989c1
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2023-08-09 18:20:29 +0200

    Imporved test script and updated worflow
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

--- Test/test_build.sh
+++ Test/test_build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+sumOfTests=0
+sumOfSuccesfulTests=0
+
+command=""
+runTestCommand () {
+    sumOfTests=$((sumOfTests+1))
+    echo "Running test command: $command"
+    eval $command
+    if (($? == 0)) ; then
+        sumOfSuccesfulTests=$((sumOfSuccesfulTests+1))
+    else
+        echo "Command $command failed"
+    fi
+}
+
+summorizeTests () {
+    if (($sumOfTests == $sumOfSuccesfulTests)) ; then
+        echo "RESULT: ALL TESTS PASSED"
+    else
+        echo "RESULT: $sumOfSuccesfulTests out of $sumOfTests passed"
+    fi
+}
+
 echo "Running test script"
 
 # Test
@@ -7,11 +30,16 @@ echo "Running test script"
 
 cd /data/workspace/WarehouseTest
 
-if cmake --build build/linux --config profile --target WarehouseTest.GameLauncher Editor ; then
-    echo "Build succeeded"
-    echo "RESULT: ALL TESTS PASSED" # expected result 
-else
-    echo "RESULT: Build failed"
-fi
+command="cmake --build build/linux --config profile --target WarehouseTest.GameLauncher Editor"
+runTestCommand
 
-exit 0
+cd ..
+command="./o3de/python/python.sh -m pytest --build-directory ./WarehouseTest/build/linux/bin/profile/ ./o3de-extras/Gems/ROS2/Code/PythonTests/SmokeTests_Periodic.py"
+runTestCommand
+
+summorizeTests
+if [ $sumOfTests -eq $sumOfSuccesfulTests ] then
+    exit 0
+else
+    exit 1
+fi
