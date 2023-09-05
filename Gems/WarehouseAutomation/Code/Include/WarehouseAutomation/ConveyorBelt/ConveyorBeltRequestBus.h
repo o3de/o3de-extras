a287d4f63cbfce696deb6e34c2b2e9c190e21d3e
Antoni-Robotec <138497503+Antoni-Robotec@users.noreply.github.com>
2023-09-05 12:13:45 +0200

    Warehouse automation gem (#440)
    Created the warehouse automation gem, adjusted to review
    
    ---------
    
    Signed-off-by: Antoni Puch <antoni.puch@robotec.ai>
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

---
+++ Gems/WarehouseAutomation/Code/Include/WarehouseAutomation/ConveyorBelt/ConveyorBeltRequestBus.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+#pragma once
+
+#include <AzCore/Component/EntityId.h>
+#include <AzCore/EBus/EBus.h>
+
+namespace WarehouseAutomation
+{
+    //! Interface class that allows to add post-processing to the pipeline
+    class ConveyorBeltRequests : public AZ::EBusTraits
+    {
+    public:
+        using BusIdType = AZ::EntityId;
+        static constexpr AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::ById;
+        static constexpr AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
+
+        //! Start a particular belt.
+        virtual void StartBelt() = 0;
+
+        //! Stop a particular belt.
+        virtual void StopBelt() = 0;
+
+        //! Query whether a particular conveyor belt is stopped.
+        virtual bool IsBeltStopped() = 0;
+
+    protected:
+        ~ConveyorBeltRequests() = default;
+    };
+
+    using ConveyorBeltRequestBus = AZ::EBus<ConveyorBeltRequests>;
+} // namespace WarehouseAutomation
