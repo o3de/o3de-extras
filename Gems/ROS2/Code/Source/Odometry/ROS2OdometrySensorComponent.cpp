9b20b27073886eb777c0a543d7cc4b1830639fab
Paweł Liberadzki <pawel.liberadzki@robotec.ai>
2023-09-13 13:35:51 +0200

    Make minor ROS2OdometrySensorComponent refactor.
    Signed-off-by: Paweł Liberadzki <pawel.liberadzki@robotec.ai>

--- Gems/ROS2/Code/Source/Odometry/ROS2OdometrySensorComponent.cpp
+++ Gems/ROS2/Code/Source/Odometry/ROS2OdometrySensorComponent.cpp
@@ -7,31 +7,26 @@
  */
 
 #include "ROS2OdometrySensorComponent.h"
-#include <AzCore/Serialization/EditContext.h>
-#include <AzCore/Serialization/EditContextConstants.inl>
 #include <AzFramework/Physics/RigidBodyBus.h>
-#include <AzFramework/Physics/SimulatedBodies/RigidBody.h>
-#include <ROS2/Frame/ROS2FrameComponent.h>
-#include <ROS2/ROS2Bus.h>
 #include <ROS2/Utilities/ROS2Conversions.h>
 #include <ROS2/Utilities/ROS2Names.h>
 
 namespace ROS2
 {
-    namespace Internal
+    namespace
     {
-        const char* kOdometryMsgType = "nav_msgs::msg::Odometry";
+        const char* OdometryMsgType = "nav_msgs::msg::Odometry";
     }
 
     void ROS2OdometrySensorComponent::Reflect(AZ::ReflectContext* context)
     {
-        if (AZ::SerializeContext* serialize = azrtti_cast<AZ::SerializeContext*>(context))
+        if (auto* serialize = azrtti_cast<AZ::SerializeContext*>(context))
         {
             serialize->Class<ROS2OdometrySensorComponent, SensorBaseType>()->Version(1);
 
-            if (AZ::EditContext* ec = serialize->GetEditContext())
+            if (auto* editContext = serialize->GetEditContext())
             {
-                ec->Class<ROS2OdometrySensorComponent>("ROS2 Odometry Sensor", "Odometry sensor component")
+                editContext->Class<ROS2OdometrySensorComponent>("ROS2 Odometry Sensor", "Odometry sensor component")
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                     ->Attribute(AZ::Edit::Attributes::Category, "ROS2")
                     ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("Game"));
@@ -40,9 +35,10 @@ namespace ROS2
     }
 
     ROS2OdometrySensorComponent::ROS2OdometrySensorComponent()
+        : m_initialTransform(AZ::Transform::CreateIdentity())
     {
         TopicConfiguration tc;
-        const AZStd::string type = Internal::kOdometryMsgType;
+        const AZStd::string type = OdometryMsgType;
         tc.m_type = type;
         tc.m_topic = "odom";
         m_sensorConfiguration.m_frequency = 10;
@@ -62,21 +58,21 @@ namespace ROS2
             AzPhysics::RigidBody* rigidBody = nullptr;
             AZ::EntityId entityId = GetEntityId();
             Physics::RigidBodyRequestBus::EventResult(rigidBody, entityId, &Physics::RigidBodyRequests::GetRigidBody);
-            AZ_Assert(rigidBody, "Entity %s does not have rigid body.", entityId.ToString().c_str());
+            AZ_Assert(rigidBody, "Entity %s does not have rigid body.", entityId.ToString().c_str())
 
             m_bodyHandle = rigidBody->m_bodyHandle;
 
             auto* sceneInterface = AZ::Interface<AzPhysics::SceneInterface>::Get();
             auto* simulatedBodyPtr = sceneInterface->GetSimulatedBodyFromHandle(sceneHandle, m_bodyHandle);
             auto* rigidbodyPtr = azrtti_cast<AzPhysics::RigidBody*>(simulatedBodyPtr);
-            AZ_Assert(rigidbodyPtr, "Requested simulated body is not a rigid body");
+            AZ_Assert(rigidbodyPtr, "Requested simulated body is not a rigid body")
             m_initialTransform = rigidbodyPtr->GetTransform();
         }
 
         auto* sceneInterface = AZ::Interface<AzPhysics::SceneInterface>::Get();
         auto* simulatedBodyPtr = sceneInterface->GetSimulatedBodyFromHandle(sceneHandle, m_bodyHandle);
         auto* rigidbodyPtr = azrtti_cast<AzPhysics::RigidBody*>(simulatedBodyPtr);
-        AZ_Assert(rigidbodyPtr, "Requested simulated body is not a rigid body");
+        AZ_Assert(rigidbodyPtr, "Requested simulated body is not a rigid body")
 
         const auto transform = rigidbodyPtr->GetTransform().GetInverse();
         const auto localAngular = transform.TransformVector(rigidbodyPtr->GetAngularVelocity());
@@ -97,9 +93,9 @@ namespace ROS2
         m_odometryMsg.header.frame_id = ROS2Names::GetNamespacedName(GetNamespace(), "odom").c_str();
         m_odometryMsg.child_frame_id = GetFrameID().c_str();
         auto ros2Node = ROS2Interface::Get()->GetNode();
-        AZ_Assert(m_sensorConfiguration.m_publishersConfigurations.size() == 1, "Invalid configuration of publishers for Odometry sensor");
+        AZ_Assert(m_sensorConfiguration.m_publishersConfigurations.size() == 1, "Invalid configuration of publishers for Odometry sensor")
 
-        const auto publisherConfig = m_sensorConfiguration.m_publishersConfigurations[Internal::kOdometryMsgType];
+        const auto publisherConfig = m_sensorConfiguration.m_publishersConfigurations[OdometryMsgType];
         const auto fullTopic = ROS2Names::GetNamespacedName(GetNamespace(), publisherConfig.m_topic);
         m_odometryPublisher = ros2Node->create_publisher<nav_msgs::msg::Odometry>(fullTopic.data(), publisherConfig.GetQoS());
 
