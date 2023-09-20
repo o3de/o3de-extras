6274ea3fbabf8b8901c200f629e66fe8a2f2c21a
Kacper Lewandowski <kacper.lewandowski@robotec.ai>
2023-09-20 12:58:19 +0200

    Added license; removed unused variable
    Signed-off-by: Kacper Lewandowski <kacper.lewandowski@robotec.ai>

--- Gems/ROS2/Code/Source/RobotImporter/FixURDF/URDFModifications.h
+++ Gems/ROS2/Code/Source/RobotImporter/FixURDF/URDFModifications.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
 
 #pragma once
 #include <AzCore/std/containers/vector.h>
@@ -16,7 +23,6 @@ namespace ROS2::Utils
     {
         AZStd::string linkName;
         AZStd::vector<AZStd::string> missingTags;
-        // AZStd::vector<AZStd::string> unnknownTags;
     };
 
     struct DuplicatedJoint
