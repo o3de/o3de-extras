63ebf851c9d9b481628fe9445a5013449b7f441a
Michał Pełka <michal.pelka@robotec.ai>
2024-01-22 10:27:18 +0100

    Adjsut after review
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ROS2/Code/Source/SimulationUtils/FollowingCameraComponent.cpp
+++ Gems/ROS2/Code/Source/SimulationUtils/FollowingCameraComponent.cpp
@@ -100,9 +100,8 @@ namespace ROS2
         InputChannelEventListener::Disconnect();
     }
 
-    AZ::Transform FollowingCameraComponent::RemoveTiltFromTransform(const AZ::Transform& transform)
+    AZ::Transform FollowingCameraComponent::RemoveTiltFromTransform(AZ::Transform transform)
     {
-        const AZ::Vector3& entityTranslation = transform.GetTranslation();
         const AZ::Vector3 axisX = transform.GetBasisX();
         const AZ::Vector3 axisY = transform.GetBasisY();
 
@@ -129,9 +128,9 @@ namespace ROS2
             newAxisY = projectedAxisY.GetNormalized();
             newAxisX = newAxisY.Cross(newAxisZ);
         }
-
-        // create new transform from the new basis vectors and the old translation
-        return AZ::Transform::CreateFromMatrix3x3AndTranslation(AZ::Matrix3x3::CreateFromColumns(projectedAxisX, projectedAxisY, newAxisZ), entityTranslation);
+        // apply rotation using created basis
+        transform.SetRotation(AZ::Quaternion::CreateFromBasis(newAxisX, newAxisY, newAxisZ));
+        return transform;
     }
 
     void FollowingCameraComponent::CacheTransform(const AZ::Transform& transform, float deltaTime)
