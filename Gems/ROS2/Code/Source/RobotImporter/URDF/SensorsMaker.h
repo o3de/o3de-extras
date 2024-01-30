24bc61f8b60979c97eb0aac78b745429702a1266
Jan Hanca <jan.hanca@robotec.ai>
2024-01-30 14:15:06 +0100

    code changes after rebase
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Gems/ROS2/Code/Source/RobotImporter/URDF/SensorsMaker.h
+++ Gems/ROS2/Code/Source/RobotImporter/URDF/SensorsMaker.h
@@ -8,12 +8,11 @@
 
 #pragma once
 
-#include "UrdfParser.h"
 #include <AzCore/Component/ComponentBus.h>
 #include <AzCore/Component/EntityId.h>
 #include <AzCore/Outcome/Outcome.h>
-#include <AzCore/std/containers/map.h>
 #include <AzCore/std/containers/set.h>
+#include <ROS2/RobotImporter/SDFormatSensorImporterHook.h>
 
 #include <sdf/sdf.hh>
 
@@ -29,14 +28,21 @@ namespace ROS2
         //! @param link A parsed SDF tree link node used to identify link being currently processed.
         //! @param entityId A non-active entity which will be affected.
         //! @return List containing any entities with sensors that were created.
-        AZStd::vector<AZ::EntityId>AddSensors(const sdf::Model& model, const sdf::Link* link, AZ::EntityId entityId);
+        AZStd::vector<AZ::EntityId> AddSensors(const sdf::Model& model, const sdf::Link* link, AZ::EntityId entityId);
 
-        //! Returns a reference to collection of status messages (read-only)
+        //! Get a reference to collection of status messages (read-only)
+        //! @return A reference to set containing status messages.
         const AZStd::set<AZStd::string>& GetStatusMessages() const;
 
     private:
         AZStd::set<AZStd::string> m_status;
 
-        void AddSensor(AZ::EntityId entityId, const sdf::Sensor* sensor);
+        using SensorHookCallOutcome = AZ::Outcome<void, AZStd::string>;
+        SensorHookCallOutcome AddSensor(AZ::EntityId entityId, const sdf::Sensor* sensor, AZStd::vector<AZ::EntityId>& createdEntities);
+        SensorHookCallOutcome CallSensorHook(
+            AZ::EntityId entityId,
+            const sdf::Sensor* sensor,
+            const SDFormat::SensorImporterHook* hook,
+            AZStd::vector<AZ::EntityId>& createdEntities);
     };
 } // namespace ROS2
