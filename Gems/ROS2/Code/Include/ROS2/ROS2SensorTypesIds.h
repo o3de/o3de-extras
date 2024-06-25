bf13ce37bd0b595dba2577ce8139a4efa96d929a
Michał Pełka <michal.pelka@robotec.ai>
2024-06-25 15:07:43 +0200

    Update ROS 2 sensors base implemtation (#715)
    - Add and tested `SensorConfigurationRequestBus`
    - Added helper functions to discover ROS 2 Sensors in runtime
    - Moved ROS 2 Sensors types to `ROS2SensorTypesIds.h`
    
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

---
+++ Gems/ROS2/Code/Include/ROS2/ROS2SensorTypesIds.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+#pragma once
+
+namespace ROS2
+{
+    inline constexpr const char* ROS2CameraSensorComponentTypeId = "{3C6B8AE6-9721-4639-B8F9-D8D28FD7A071}";
+    inline constexpr const char* ROS2ContactSensorComponentTypeId = "{91272e66-c9f1-4aa2-a9d5-98eaa4ef4e9a}";
+    inline constexpr const char* ROS2GNSSSensorComponentTypeId = "{55B4A299-7FA3-496A-88F0-764C75B0E9A7}";
+    inline constexpr const char* ROS2ImuSensorComponentTypeId = "{502A955E-7742-4E23-AD77-5E4063739DCA}";
+    inline constexpr const char* ROS2LidarSensorComponentTypeId = "{502A955F-7742-4E23-AD77-5E4063739DCA}";
+    inline constexpr const char* ROS2Lidar2DSensorComponentTypeId = "{F4C2D970-1D69-40F2-9D4D-B52DCFDD2704}";
+    inline constexpr const char* ROS2WheelOdometryComponentTypeId = "{9bdb8c23-ac76-4c25-8d35-37aaa9f43fac}";
+    inline constexpr const char* ROS2OdometrySensorComponent = "{61387448-63AA-4563-AF87-60C72B05B863}";
+} // namespace ROS2
