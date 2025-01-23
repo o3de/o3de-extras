92b51967da53447e3579d3f38e2ef1b8faba9c30
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2025-01-23 15:58:08 +0100

    Fix bug where joints werent clearing in  JointPositionEditorComponent (#814)
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

---
+++ Gems/ROS2/Code/Source/Manipulation/JointUtils.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ *
+ */
+
+#pragma once
+
+#include <AzCore/Component/Entity.h>
+
+namespace ROS2::JointUtils
+{
+    //! Check if the entity has any of the non fixed joint or articulation components.
+    //! @param entity Entity to check.
+    //! @return True if the entity has any of the joint or articulation components.
+    bool HasNonFixedJoints(const AZ::Entity* entity);
+} // namespace ROS2::Utils
