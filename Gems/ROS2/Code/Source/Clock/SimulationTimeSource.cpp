1f3babb79fc93776a47511cfefdb12717150cc85
Jan Hanca <jan.hanca@robotec.ai>
2025-01-24 12:33:58 +0100

    Clang-format; add printf; fix log messages
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Gems/ROS2/Code/Source/Clock/SimulationTimeSource.cpp
+++ Gems/ROS2/Code/Source/Clock/SimulationTimeSource.cpp
@@ -6,16 +6,18 @@
  *
  */
 
+#include <AzCore/Settings/SettingsRegistry.h>
 #include <AzCore/Time/ITime.h>
 #include <ROS2/Clock/SimulationTimeSource.h>
 #include <ROS2/ROS2Bus.h>
-#include <AzCore/Settings/SettingsRegistry.h>
 
 namespace ROS2
 {
-    namespace {
+    namespace
+    {
         constexpr AZStd::string_view ResetTimestampOnLevelReload = "/O3DE/ROS2/SteadyClock/ResetTimestampOnLevelReload";
     }
+
     void SimulationTimeSource::Activate()
     {
         auto* registry = AZ::SettingsRegistry::Get();
@@ -28,7 +30,7 @@ namespace ROS2
         auto* systemInterface = AZ::Interface<AzPhysics::SystemInterface>::Get();
         if (!systemInterface)
         {
-            AZ_Warning("SimulationPhysicalClock", false, "Failed to get AzPhysics::SystemInterface");
+            AZ_Warning("SimulationTimeSource", false, "Failed to get AzPhysics::SystemInterface");
             return;
         }
         m_onSceneSimulationEvent = AzPhysics::SceneEvents::OnSceneSimulationFinishHandler(
@@ -40,14 +42,14 @@ namespace ROS2
         m_onSceneAdded = AzPhysics::SystemEvents::OnSceneAddedEvent::Handler(
             [this](AzPhysics::SceneHandle sceneHandle)
             {
-
                 auto* sceneInterface = AZ::Interface<AzPhysics::SceneInterface>::Get();
                 AzPhysics::SceneHandle defaultSceneHandle = sceneInterface->GetSceneHandle(AzPhysics::DefaultPhysicsSceneName);
                 if (sceneHandle == defaultSceneHandle)
                 {
-                    AZ_Printf("SimulationPhysicalClock", "Registering clock to default scene");
+                    AZ_Printf("SimulationTimeSource", "Registering clock to default scene");
                     if (m_resetTimeOnRestart)
                     {
+                        AZ_Printf("SimulationTimeSource", "Reseting clock");
                         m_elapsed = 0.0;
                     }
                     sceneInterface->RegisterSceneSimulationFinishHandler(sceneHandle, m_onSceneSimulationEvent);
@@ -62,7 +64,7 @@ namespace ROS2
                 AzPhysics::SceneHandle defaultSceneHandle = sceneInterface->GetSceneHandle(AzPhysics::DefaultPhysicsSceneName);
                 if (sceneHandle == defaultSceneHandle)
                 {
-                    AZ_Printf("SimulationPhysicalClock", "Removing clock to default scene");
+                    AZ_Printf("SimulationTimeSource", "Removing clock to default scene");
                     m_onSceneSimulationEvent.Disconnect();
                 }
             });
