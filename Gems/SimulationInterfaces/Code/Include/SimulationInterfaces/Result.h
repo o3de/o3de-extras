0b976164bee87a1fef65672735cde6b84263cd3b
Michał Pełka <michal.pelka@robotec.ai>
2025-04-08 16:23:58 +0200

    Adjust error code type
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/SimulationInterfaces/Code/Include/SimulationInterfaces/Result.h
+++ Gems/SimulationInterfaces/Code/Include/SimulationInterfaces/Result.h
@@ -13,24 +13,34 @@ namespace SimulationInterfaces
 {
     //! Result codes to be used in the Result message
     //!  @see <a href="https://github.com/ros-simulation/simulation_interfaces/blob/main/msg/Result.msg">Result.msg</a>
-    enum class ErrorCode
+    using ErrorCodeValue = uint8_t;
+    namespace ErrorCode
     {
-        RESULT_FEATURE_UNSUPPORTED = 0,
-        RESULT_NOT_FOUND = 2,
-        RESULT_INCORRECT_STATE = 3,
-        RESULT_OPERATION_FAILED = 4
-    };
+        constexpr ErrorCodeValue RESULT_FEATURE_UNSUPPORTED = 0;
+        constexpr ErrorCodeValue RESULT_OK = 1;
+        constexpr ErrorCodeValue RESULT_NOT_FOUND = 2;
+        constexpr ErrorCodeValue RESULT_INCORRECT_STATE = 3;
+        constexpr ErrorCodeValue RESULT_OPERATION_FAILED = 4;
 
+        inline bool IsSuccess(ErrorCodeValue v)
+        {
+            return v == RESULT_OK;
+        }
+        inline bool IsMessageSpecific(ErrorCodeValue v)
+        {
+            return v > 100;
+        }
+    }; // namespace ErrorCode
     //! A message type to represent the result of a failed operation
     struct FailedResult
     {
         FailedResult() = default;
-        FailedResult(ErrorCode error_code, const AZStd::string& error_string)
+        FailedResult(ErrorCodeValue error_code, const AZStd::string& error_string)
             : error_code(error_code)
             , error_string(error_string)
         {
         }
-        ErrorCode error_code;
+        ErrorCodeValue error_code;
         AZStd::string error_string;
     };
 } // namespace SimulationInterfaces
