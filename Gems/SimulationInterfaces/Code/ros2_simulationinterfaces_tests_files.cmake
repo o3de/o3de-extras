85399cd87cccf02a6baaefd2f506011868058032
Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>
2025-04-10 16:23:15 +0200

    rebase missed change added
    Signed-off-by: Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>

--- Gems/SimulationInterfaces/Code/ros2_simulationinterfaces_tests_files.cmake
+++ Gems/SimulationInterfaces/Code/ros2_simulationinterfaces_tests_files.cmake
@@ -5,6 +5,7 @@
 #
 
 set(FILES
+    Tests/Tools/Mocks/SimulationManagerMock.h
     Tests/Tools/Mocks/SimulationEntityManagerMock.h
     Tests/Tools/Mocks/SimulationFeaturesAggregatorRequestsHandlerMock.h
     Tests/Tools/InterfacesTest.cpp
