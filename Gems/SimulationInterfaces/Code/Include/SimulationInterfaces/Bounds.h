0afbc9e5ca8366145386540e3ff264604b94abd9
Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>
2025-05-05 11:56:59 +0200

    Types aliases changed in the same manner as in the rest of the gem
    Signed-off-by: Norbert Prokopiuk <norbert.prokopiuk@robotec.ai>

--- Gems/SimulationInterfaces/Code/Include/SimulationInterfaces/Bounds.h
+++ Gems/SimulationInterfaces/Code/Include/SimulationInterfaces/Bounds.h
@@ -7,15 +7,17 @@
  */
 
 #pragma once
-#include "AzCore/Math/Vector3.h"
+#include <AzCore/Math/Vector3.h>
 #include <AzCore/Outcome/Outcome.h>
 #include <AzCore/std/containers/vector.h>
 #include <AzCore/std/string/string.h>
+#include <simulation_interfaces/msg/bounds.hpp>
+
 namespace SimulationInterfaces
 {
     //! Bounds types to be used in the Bounds message
     //!  @see <a href="https://github.com/ros-simulation/simulation_interfaces/blob/main/msg/Bounds.msg">Result.msg</a>
-    using BoundsType = uint8_t;
+    using BoundsType = simulation_interfaces::msg::Bounds::_type_type;
 
     //! A message type to represent the bounds
     struct Bounds
