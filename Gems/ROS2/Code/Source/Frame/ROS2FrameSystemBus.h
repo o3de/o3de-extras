aeec92fa7274bc44d1570d17bd0178b2a503b477
Jan Hanca <jan.hanca@robotec.ai>
2025-05-26 17:28:54 +0200

    Add ROS2FrameSystemComponent to asset processor
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Gems/ROS2/Code/Source/Frame/ROS2FrameSystemBus.h
+++ Gems/ROS2/Code/Source/Frame/ROS2FrameSystemBus.h
@@ -7,13 +7,13 @@
  */
 #pragma once
 
+#include <AzCore/Component/EntityId.h>
+#include <AzCore/EBus/EBus.h>
 #include <AzCore/EBus/Policies.h>
 #include <AzCore/Interface/Interface.h>
 #include <AzCore/RTTI/RTTIMacros.h>
 #include <AzCore/std/string/string.h>
 #include <ROS2/Frame/ROS2FrameComponent.h>
-#include <AzCore/Component/EntityId.h>
-#include <AzCore/EBus/EBus.h>
 
 namespace ROS2
 {
@@ -66,7 +66,7 @@ namespace ROS2
     {
     public:
         static constexpr AZ::EBusHandlerPolicy HandlerPolicy = AZ::EBusHandlerPolicy::Single;
-        static constexpr AZ::EBusHandlerPolicy AddressPolicy = AZ::EBusHandlerPolicy::Single;
+        static constexpr AZ::EBusAddressPolicy AddressPolicy = AZ::EBusAddressPolicy::Single;
     };
 
     using ROS2FrameSystemInterface = AZ::Interface<ROS2FrameSystemRequests>;
