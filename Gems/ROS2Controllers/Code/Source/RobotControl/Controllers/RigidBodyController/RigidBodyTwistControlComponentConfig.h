8ed103f55f698540b08a9fffb3edc1e3b2ee1212
Jan Hanca <jan.hanca@robotec.ai>
2025-07-17 09:33:04 +0200

    clang-format the code
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Gems/ROS2Controllers/Code/Source/RobotControl/Controllers/RigidBodyController/RigidBodyTwistControlComponentConfig.h
+++ Gems/ROS2Controllers/Code/Source/RobotControl/Controllers/RigidBodyController/RigidBodyTwistControlComponentConfig.h
@@ -8,15 +8,18 @@
 #pragma once
 
 #include <AzCore/RTTI/TypeInfo.h>
-#include <AzCore/Serialization/SerializeContext.h>
 #include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/SerializeContext.h>
 #include <ROS2Controllers/Controllers/PidConfiguration.h>
 
-namespace ROS2Controllers {
+namespace ROS2Controllers
+{
     //! Configuration class for RigidBodyTwistControlComponent
-    class RigidBodyTwistControlComponentConfig {
+    class RigidBodyTwistControlComponentConfig
+    {
     public:
-        enum class PhysicalApi {
+        enum class PhysicalApi
+        {
             Velocity,
             Kinematic,
             Force
@@ -24,25 +27,25 @@ namespace ROS2Controllers {
 
         AZ_TYPE_INFO(RigidBodyTwistControlComponentConfig, "{8F2E9C4B-1D5A-4B7C-9E8F-3A6B5C7D8E9F}");
 
-        static void Reflect(AZ::ReflectContext *context);
+        static void Reflect(AZ::ReflectContext* context);
 
         // Visibility functions for edit context
-        [[nodiscard]] AZ::u32 GetPidControllersVisibility() const {
-            return m_physicalApi == PhysicalApi::Force ? AZ::Edit::PropertyVisibility::Show
-                                                       : AZ::Edit::PropertyVisibility::Hide;
+        [[nodiscard]] AZ::u32 GetPidControllersVisibility() const
+        {
+            return m_physicalApi == PhysicalApi::Force ? AZ::Edit::PropertyVisibility::Show : AZ::Edit::PropertyVisibility::Hide;
         }
 
         AZStd::string GetNote() const;
 
         //! Pid controllers for linear velocities - default config for each axis
-        AZStd::array<PidConfiguration, 3> m_linerControllers =
-                {PidConfiguration(500, 50, 0, 0, 0, false, false), PidConfiguration(500, 50, 0, 0, 0, false, false),
-                 PidConfiguration(0, 0, 0, 0, 0, false, false)};
+        AZStd::array<PidConfiguration, 3> m_linerControllers = { PidConfiguration(500, 50, 0, 0, 0, false, false),
+                                                                 PidConfiguration(500, 50, 0, 0, 0, false, false),
+                                                                 PidConfiguration(0, 0, 0, 0, 0, false, false) };
 
         //! Pid controllers for angular velocities - default config for each axis
-        AZStd::array<PidConfiguration, 3> m_angularControllers = {PidConfiguration(0, 0, 0, 0, 0, false, false),
-                                                                  PidConfiguration(0, 0, 0, 0, 0, false, false),
-                                                                  PidConfiguration(500, 50, 0, 0, 0, false, false)};
+        AZStd::array<PidConfiguration, 3> m_angularControllers = { PidConfiguration(0, 0, 0, 0, 0, false, false),
+                                                                   PidConfiguration(0, 0, 0, 0, 0, false, false),
+                                                                   PidConfiguration(500, 50, 0, 0, 0, false, false) };
         PhysicalApi m_physicalApi = PhysicalApi::Velocity; //!< API to use for applying velocities
     };
 } // namespace ROS2Controllers
