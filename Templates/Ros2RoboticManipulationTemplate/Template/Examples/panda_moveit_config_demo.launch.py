6a191ac737fa0a6ec0f260bc578e51066b8ad142
Jan Hanca <jan.hanca@robotec.ai>
2026-04-09 14:11:32 +0200

    Update Panda MoveIt example
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

--- Templates/Ros2RoboticManipulationTemplate/Template/Examples/panda_moveit_config_demo.launch.py
+++ Templates/Ros2RoboticManipulationTemplate/Template/Examples/panda_moveit_config_demo.launch.py
@@ -1,12 +1,9 @@
 from pathlib import Path
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import OpaqueFunction
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import ExecuteProcess
-from ament_index_python.packages import get_package_share_directory
+
 from moveit_configs_utils import MoveItConfigsBuilder
 
 from launch_ros.actions import ComposableNodeContainer
@@ -14,12 +11,7 @@ from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-
-    declared_arguments = []
-
-    return LaunchDescription(
-        declared_arguments + [OpaqueFunction(function=launch_setup)]
-    )
+    return LaunchDescription([OpaqueFunction(function=launch_setup)])
 
 
 def launch_setup(context, *args, **kwargs):
@@ -27,7 +19,7 @@ def launch_setup(context, *args, **kwargs):
     current_file_path = Path(__file__).resolve()
     current_directory = str(current_file_path.parent)
 
-    use_sim_time = { "use_sim_time": True}
+    use_sim_time = {"use_sim_time": True}
     moveit_config = (
         MoveItConfigsBuilder("moveit_resources_panda")
         .robot_description(file_path="config/panda.urdf.xacro")
@@ -48,11 +40,9 @@ def launch_setup(context, *args, **kwargs):
         parameters=[moveit_config.to_dict() | use_sim_time],
     )
 
-    rviz_base = LaunchConfiguration("rviz_config")
     rviz_config = PathJoinSubstitution(
         [current_directory, "panda_moveit_config_demo.rviz"]
     )
-
 
     # RViz
     rviz_node = Node(
@@ -80,25 +70,28 @@ def launch_setup(context, *args, **kwargs):
     )
 
     rgbd_pc = ComposableNodeContainer(
-            name='container0',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='depth_image_proc',
-                    plugin='depth_image_proc::PointCloudXyzrgbNode',
-                    name='point_cloud_xyzrgb_node',
-                    remappings=[('rgb/camera_info', '/color_camera_info'),
-                                ('rgb/image_rect_color', '/camera_image_color'),
-                                ('depth_registered/image_rect','/camera_image_depth'),
-                                ('/points','/pointcloud')]
-                ),
-            ],
-            output='screen',
-            parameters=[{'use_sim_time': True}],
-        )
-    
+        name="container0",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="depth_image_proc",
+                plugin="depth_image_proc::PointCloudXyzrgbNode",
+                name="point_cloud_xyzrgb_node",
+                remappings=[
+                    ("rgb/camera_info", "/color_camera_info"),
+                    ("/camera_info", "/color_camera_info"),
+                    ("rgb/image_rect_color", "/camera_image_color"),
+                    ("depth_registered/image_rect", "/camera_image_depth"),
+                    ("/points", "/pointcloud"),
+                ],
+                parameters=[use_sim_time],
+            ),
+        ],
+        output="screen",
+    )
+
     # Publish TF
     robot_state_publisher = Node(
         package="robot_state_publisher",
@@ -107,7 +100,7 @@ def launch_setup(context, *args, **kwargs):
         output="both",
         parameters=[moveit_config.robot_description],
     )
-   
+
     nodes_to_start = [
         rviz_node,
         static_tf,
