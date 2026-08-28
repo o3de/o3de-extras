8ef8cdf3c697a96a39fed09f49d6bbc114a1c84c
Mateusz Żak <mateusz.zak@robotec.ai>
2026-08-28 09:40:15 +0200

    Use O3DE's 3rdParty convention for ROS 2 package dependencies to survive in Prebuilt Gems (#1066)
    * Move FindROS2.cmake to the ROS2 gem's 3rdParty path
    * Resolve ros2 package dependencies as 3rdParty::ROS2 targets
    * Remove the now-unused Code/ module-path registration
    
    Signed-off-by: Mateusz Żak <mateusz.zak@robotec.ai>
    (cherry picked from commit 82c4610795338c466ee36247b7a5d2fb994e0dfc)

--- Gems/ROS2/Code/ros2_target_depends.cmake
+++ Gems/ROS2/Code/ros2_target_depends.cmake
@@ -3,17 +3,32 @@
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
+# Record dependencies on ROS 2 packages through O3DE's native model:
+# wrap as a 3rdParty::ROS2::<pkg> target and link with ly_target_link_libraries.
+# As a result Install_common.cmake can see those.
+# Prebuilt Gems can provide these names in their generated cmake;
+# consumers re-resolve them  against the locally installed packages via 3rdParty/FindROS2.cmake.
+# Kept in sync with 3rdParty/FindROS2.cmake, which does the same wrapping at the # consumer's configure. 
+function(_ros2_wrap_ament_package _package)
+    if (TARGET 3rdParty::ROS2::${_package})
+        return()
+    endif ()
+    include(${${_package}_DIR}/${_package}Config.cmake OPTIONAL)
+    if (${${_package}_FOUND_AMENT_PACKAGE})
+        message(DEBUG "Package ${_package} was found (${${_package}_DIR}) version ${${_package}_VERSION} targets : ${${_package}_TARGETS}")
+        add_library(3rdParty::ROS2::${_package} INTERFACE IMPORTED GLOBAL)
+        target_link_libraries(3rdParty::ROS2::${_package} INTERFACE ${${_package}_TARGETS})
+    else ()
+        message(FATAL_ERROR "Package ${_package} was found (${${_package}_DIR}), but package is not an Ament package.")
+    endif ()
+endfunction()
+
 function(target_depends_on_ros2_package TARGET_NAME)
     list(GET ARGN 0 _package)
     find_package(${ARGN})
     if (${${_package}_FOUND})
-        include(${${_package}_DIR}/${_package}Config.cmake OPTIONAL)
-        if (${${_package}_FOUND_AMENT_PACKAGE})
-            message(DEBUG "Package ${_package} was found (${${_package}_DIR}) version ${${_package}_VERSION} targets : ${${_package}_TARGETS}")
-            target_link_libraries(${TARGET_NAME} PUBLIC ${${_package}_TARGETS})
-        else ()
-            message(FATAL_ERROR "Package ${_package} was found (${${_package}_DIR}), but package is not an Ament package.")
-        endif ()
+        _ros2_wrap_ament_package(${_package})
+        ly_target_link_libraries(${TARGET_NAME} PUBLIC 3rdParty::ROS2::${_package})
     else ()
         message(DEBUG "Package ${_package} was not found.")
     endif ()
@@ -25,17 +40,11 @@ function(target_depends_on_ros2_packages TARGET_NAME)
     endforeach ()
 endfunction()
 
-
 function(target_interface_depends_on_ros2_package TARGET_NAME)
     list(GET ARGN 0 _package)
     find_package(${ARGN})
-    include(${${_package}_DIR}/${_package}Config.cmake OPTIONAL)
-    if (${${_package}_FOUND_AMENT_PACKAGE})
-        message(DEBUG "Package ${_package} was found (${${_package}_DIR}) version ${${_package}_VERSION} targets : ${${_package}_TARGETS}")
-        target_link_libraries(${TARGET_NAME} INTERFACE ${${_package}_TARGETS})
-    else ()
-        message(FATAL_ERROR "Package ${_package} was found (${${_package}_DIR}), but package is not an Ament package.")
-    endif ()
+    _ros2_wrap_ament_package(${_package})
+    ly_target_link_libraries(${TARGET_NAME} INTERFACE 3rdParty::ROS2::${_package})
 endfunction()
 
 function(target_interface_depends_on_ros2_packages TARGET_NAME)
